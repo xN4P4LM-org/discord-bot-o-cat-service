058ccdcdd1e4112d34154b8dd4c7b230b9d7fe34
Diana <dmoore@xn4p4lm.com>
2024-02-08 02:25:52 +0000

    Remove CodeQL support and update pip requirements path
    Signed-off-by: Diana <dmoore@xn4p4lm.com>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,7 +4,7 @@
 pip install --upgrade pip
 
 # install pip dev dependencies
-pip install -r requirements-dev.txt
+pip install -r bot/requirements-dev.txt
 
 # welcome message
 echo "Welcome to the bot-o-cat development environment!"
