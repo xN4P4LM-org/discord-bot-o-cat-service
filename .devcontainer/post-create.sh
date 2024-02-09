6681afb5bd68b09360ca8d1b119317c5498652dd
Diana <dmoore@xn4p4lm.com>
2024-02-09 05:37:38 +0000

    Remove git.branchPrefix and update submodule pull command
    Signed-off-by: Diana <dmoore@xn4p4lm.com>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,11 +3,11 @@
 # upgrade pip
 pip install --upgrade pip
 
+# pull most recent submodules
+GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git submodule update --init --recursive
+
 # install pip dev dependencies
 pip install -r bot/requirements-dev.txt
-
-# pull most recent submodules
-git submodule update --init --recursive
 
 # welcome message
 echo "Welcome to the bot-o-cat development environment!"
